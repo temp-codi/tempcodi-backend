1a1db82852513f0eabde0131a44685b7f8b0750f
pol-dev-shinroo <manus.dream.language@gmail.com>
2023-01-29 01:20:38 +0900

    render deploy

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Application } from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
 import morgan from 'morgan';
+import rateLimit from 'express-rate-limit';
 import compression from 'compression';
 
 class App {
@@ -13,6 +14,7 @@ class App {
         this.port = port;
 
         this.initializeMiddleware();
+        this.initializeHome();
     }
 
     private initializeMiddleware(): void {
@@ -21,10 +23,21 @@ class App {
         this.express.use(morgan('dev'));
         this.express.use(express.json());
         this.express.use(express.urlencoded({ extended: false }));
+        this.express.set('trust proxy', 1);
+        this.express.use(
+            rateLimit({
+                windowMs: 15 * 60 * 1000, // 15 minutes
+                max: 100, // limit each IP to 100 requests per windowMs
+            })
+        );
         this.express.use(compression());
     }
 
-    private initializeHome(): void {}
+    private initializeHome(): void {
+        this.express.get('/', (req, res) => {
+            res.send('<h1>TEMPCODI API</h1>');
+        });
+    }
 
     public listen(): void {
         this.express.listen(this.port, () => {
