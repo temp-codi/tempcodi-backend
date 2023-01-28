ee4242c2b61b9fb3174a4ac8440f96aef4d81b4b
pol-dev-shinroo <manus.dream.language@gmail.com>
2023-01-29 03:04:39 +0900

    validation handling implemented on geolocation routes

--- src/controllers/geolocation/geolocation.controller.ts
+++ src/controllers/geolocation/geolocation.controller.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import Controller from '@/utils/interfaces/controller';
 import asyncWrapper from '@/middlewares/async';
+import validate from './geolocation.validation';
+import validationMiddleware from '@/middlewares/validation';
 
 class GeolocationController implements Controller {
     public path = '/geolocation';
@@ -11,7 +13,9 @@ class GeolocationController implements Controller {
     }
 
     private initializeRoutes(): void {
-        this.router.route(this.path).post(this.findCity);
+        this.router
+            .route(this.path)
+            .post(validationMiddleware(validate.create), this.findCity);
     }
 
     private findCity = asyncWrapper(async (req, res, next) => {});
