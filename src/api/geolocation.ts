6eecd7fef97713eb14c531143e8e6596e929f3cb
pol-dev-shinroo <manus.dream.language@gmail.com>
2023-01-29 18:59:01 +0900

    geolocation api refactoring

--- src/api/geolocation.ts
+++ src/api/geolocation.ts
@@ -7,7 +7,7 @@ const reverseGeoApi = async (lat: string, lon: string) => {
             results: [{ components }],
         },
     } = await axios.get(
-        `https://api.opencagedata.com/geocode/v1/json?key=e8c5f280f4f64f7c93e8634aac3ffaaf&q=52.3877830%2C9.7334394&pretty=1`
+        `https://api.opencagedata.com/geocode/v1/json?q=${lat}+${lon}&key=${process.env.OPEN_CAGE_API_KEY}`
     );
     return components;
 };
