bec68c821ae7113de36d53f3402551a76ac65b6e
pol-dev-shinroo <manus.dream.language@gmail.com>
2023-02-01 18:03:23 +0900

    weather mongoose model setup

---
+++ src/controllers/weather/weather.model.ts
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const CitySchema = new mongoose.Schema({
+    city_name: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    api_called_date: { type: String, required: true },
+    list: [
+        {
+            dt: { type: Number, required: true },
+            temp: {
+                type: Number,
+                required: true,
+            },
+            feels_like: { type: Number, required: true },
+            humidity: { type: Number, required: true },
+            cloud_in_percentage: { type: Number, required: true },
+            wind_speed: { type: Number, required: true },
+            weather_id: { type: Number, required: true },
+        },
+    ],
+    pollution_en: { type: String, required: true },
+    pollution_kr: { type: String, required: true },
+});
+
+module.exports = mongoose.set('strictQuery', true).model('cities', CitySchema);
